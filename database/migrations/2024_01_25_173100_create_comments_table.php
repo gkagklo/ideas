44383f80868c4072ade12cd034da35dadef87758
gkagklo <deathbreakerxx@hotmail.com>
2024-01-26 15:20:11 +0200

    authorization + route grouping

--- database/migrations/2024_01_25_173100_create_comments_table.php
+++ database/migrations/2024_01_25_173100_create_comments_table.php
@@ -15,6 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('idea_id')->constrained()->cascadeOnDelete();
             $table->string('content');    
             $table->timestamps();
