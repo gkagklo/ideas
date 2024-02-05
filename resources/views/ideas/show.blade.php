c1275b819fcd2e4913056df7a04f956bdfa52651
gkagklo <deathbreakerxx@hotmail.com>
2024-02-05 19:14:34 +0200

    feed page

--- resources/views/ideas/show.blade.php
+++ resources/views/ideas/show.blade.php
@@ -7,7 +7,7 @@
             <div class="col-6">
                 @include('shared.success-message')      
                 <div class="mt-3">
-                    @include('ides.shared.idea-card')
+                    @include('ideas.shared.idea-card')
                 </div>    
             </div>
             <div class="col-3">
